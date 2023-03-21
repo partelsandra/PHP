5b1b7b9c321bdb22cf1dda73ab84d10528a9c6bb
Sandra Partel <sandra.partel.001@voco.ee>
2023-03-21 10:12:58 +0200

    first commit

---
+++ update-process.php
@@ -0,0 +1,31 @@
+
+<html>
+<head>
+<link rel="stylesheet" href="style.css">
+<title>Uuenda ISIKUD Andmeid</title>
+</head>
+<body>
+<form name="frmUser" method="post" action="">
+<div></div>
+<div style="padding-bottom:5px;">
+<a href="retrieve.php">Isikute List</a>
+</div>
+Nimi: <br>
+<input type="text" name="Nimi" class="txtField" value="Aadu">
+<br>
+sugu: <br>
+<input type="text" name="sugu" class="txtField" value="M">
+<br>
+pikkus :<br>
+<input type="int" name="pikkus" class="txtField" value="187">
+<br>
+kaal:<br>
+<input type="int" name="kaal" class="txtField" value="89">
+<br>
+synniaeg:<br>
+<input type="date" name="synniaeg" class="txtField" value="1976-02-14">
+<br>
+<input type="submit" name="submit" value="Submit" class="button">
+</form>
+</body>
+</html>
