4822b5832b039c93bd4646e717c06b7b07cecb3a
Sandra Partel <sandra.partel.001@voco.ee>
2023-03-28 21:39:13 +0300

    update

--- retrieve.php
+++ retrieve.php
@@ -11,7 +11,6 @@
 
 <body>
     <table>
-
         <tr>
             <td>Nimi </td>
             <td>sugu </td>
@@ -21,6 +20,30 @@
             <td>Action </td>
             <td>Action </td>
         </tr>
+
+        <?php
+        require_once('database.php');
+
+        $sql = "SELECT * FROM isikud";
+        $result = $conn->query($sql);
+
+        if ($result->num_rows > 0) {
+            while ($row = $result->fetch_assoc()) {
+                echo "<tr>
+            <td>" . $row["Nimi"] . "</td>
+            <td>" . $row["sugu"] . "</td>
+            <td>" . $row["pikkus"] . "</td>
+            <td>" . $row["kaal"] . "</td>
+            <td>" . $row["synniaeg"] . "</td>
+            <td><a href='edit.php?id=" . $row["id"] . "'>Edit</a></td>
+            <td><a href='delete.php?id=" . $row["id"] . "'>Delete</a></td>
+            </tr>";
+            }
+        } else {
+            echo "<tr><td colspan='7'>No results found</td></tr>";
+        }
+        $conn->close();
+        ?>
 
     </table>
 </body>
