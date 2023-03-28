fcb43b8e884a462441a2785d680fef11eefdd6e9
Sandra Partel <sandra.partel.001@voco.ee>
2023-03-28 21:09:57 +0300

    update

--- process.php
+++ process.php
@@ -1,14 +1,14 @@
 <?php
-require_once('database.php');
+include_once('database.php');
 if(isset($_POST['submit']))
 {	 
-	 $nimi = $_POST['nimi'];
+	 $nimi = $_POST['Nimi'];
 	 $sugu = $_POST['sugu'];
 	 $pikkus = $_POST['pikkus'];
 	 $kaal = $_POST['kaal'];
 	 $synniaeg = $_POST['synniaeg'];
-	 $sql = "INSERT INTO ISIKUD (nimi,sugu,pikkus,kaal,synniaeg)
-	 VALUES ('$nimi','$sugu','$pikkus','$kaal', '$synniaeg')";
+	 $sql = "INSERT INTO ISIKUD (Nimi,sugu,pikkus,kaal,synniaeg)
+	 VALUES ('$nimi','$sugu','$pikkus','$kaal','$synniaeg')";
 	 if (mysqli_query($conn, $sql)) {
 		echo "New record created successfully !";
 	 } else {
--- retrieve.php
+++ retrieve.php
@@ -1,95 +1,28 @@
-
 <!DOCTYPE html>
 <html>
- <head>
- <link rel="stylesheet" href="style.css">
- <title>Taasta andmed</title>
- <a href="insert.php"> <p>Lisa isik</p> </a>
- </head>
+
+<head>
+    <link rel="stylesheet" href="style.css">
+    <title>Taasta andmed</title>
+    <a href="insert.php">
+        <p>Lisa isik</p>
+    </a>
+</head>
+
 <body>
-  <table>
-  
-  <tr>
-    <td>Nimi </td>
-    <td>sugu </td>
-    <td>pikkus </td>
-    <td>kaal </td>
-	<td>synniaeg </td>
-	<td>Action </td>
-	<td>Action </td>
-  </tr>
-<tr>
-    <td>Aadu</td>
-    <td>M</td>
-    <td>187</td>
-    <td>89</td>
-	<td>1976-02-14</td>
-	<td><a href = "update-process.php?Isiku_id=8">Update</a></td>
-	<td><a href="delete-process.php?Isiku_id=8">Delete</a></td>
-</tr>
-<tr>
-    <td>Peeter</td>
-    <td>M</td>
-    <td>190</td>
-    <td>75</td>
-	<td>1975-12-02</td>
-	<td><a href = "update-process.php?Isiku_id=9">Update</a></td>
-	<td><a href="delete-process.php?Isiku_id=9">Delete</a></td>
-</tr>
-<tr>
-    <td>Tiit</td>
-    <td>M</td>
-    <td>168</td>
-    <td>59</td>
-	<td>2000-02-17</td>
-	<td><a href = "update-process.php?Isiku_id=25">Update</a></td>
-	<td><a href="delete-process.php?Isiku_id=25">Delete</a></td>
-</tr>
-<tr>
-    <td>Hanno</td>
-    <td>M</td>
-    <td>178</td>
-    <td>100</td>
-	<td>1976-11-12</td>
-	<td><a href = "update-process.php?Isiku_id=11">Update</a></td>
-	<td><a href="delete-process.php?Isiku_id=11">Delete</a></td>
-</tr>
-<tr>
-    <td>Heiki</td>
-    <td>M</td>
-    <td>193</td>
-    <td>97</td>
-	<td>1975-10-29</td>
-	<td><a href = "update-process.php?Isiku_id=12">Update</a></td>
-	<td><a href="delete-process.php?Isiku_id=12">Delete</a></td>
-</tr>
-<tr>
-    <td>Tiiu</td>
-    <td>N</td>
-    <td>85</td>
-    <td>90</td>
-	<td>2023-01-01</td>
-	<td><a href = "update-process.php?Isiku_id=13">Update</a></td>
-	<td><a href="delete-process.php?Isiku_id=13">Delete</a></td>
-</tr>
-<tr>
-    <td>Mari</td>
-    <td>N</td>
-    <td>75</td>
-    <td>95</td>
-	<td>2023-01-29</td>
-	<td><a href = "update-process.php?Isiku_id=14">Update</a></td>
-	<td><a href="delete-process.php?Isiku_id=14">Delete</a></td>
-</tr>
-<tr>
-    <td>Mari</td>
-    <td>N</td>
-    <td>180</td>
-    <td>75</td>
-	<td>2002-08-14</td>
-	<td><a href = "update-process.php?Isiku_id=15">Update</a></td>
-	<td><a href="delete-process.php?Isiku_id=15">Delete</a></td>
-</tr>
-</table>
-  </body>
+    <table>
+
+        <tr>
+            <td>Nimi </td>
+            <td>sugu </td>
+            <td>pikkus </td>
+            <td>kaal </td>
+            <td>synniaeg </td>
+            <td>Action </td>
+            <td>Action </td>
+        </tr>
+
+    </table>
+</body>
+
 </html>
